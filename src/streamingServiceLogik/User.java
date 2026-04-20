1696577388956636aa75316873ce8a7c5b614083
phili <phtr1000@stud.ek.dk>
2026-04-20 12:48:48 +0200

    mapperosv

--- src/streamingServiceLogik/User.java
+++ src/streamingServiceLogik/User.java
@@ -1,17 +1,67 @@
 package streamingServiceLogik;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static sun.security.util.KnownOIDs.Data;
+
 
 public class User {
 
-    String userName;
+    private String userName;
+    private String password;
+    private ArrayList<Media> savedMedia;
+    private ArrayList<Media> watchedMovies;
 
-    //midlertidig constructor max tilføjede
     public User(String userName, String userPassWord) {
-
         this.userName = userName;
+        this.password = userPassWord;
+        this.savedMedia = new ArrayList<>();
+        this.watchedMovies = new ArrayList<>();
     }
+
+
+
 
 
     public String getUserName() {
         return userName;
     }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void addWatchedMovies(Media media) {
+        if (!watchedMovies.contains(media)) {
+            watchedMovies.add(media);
+        }
+    }
+
+    public void addSavedMedia(Media media) {
+        if (!savedMedia.contains(media)) {
+            savedMedia.add(media);
+        }
+    }
+
+    public void removeSavedMedia(Media media) {
+        savedMedia.remove(media);
+    }
+
+    public ArrayList<Media> getSavedMedia() {
+        return savedMedia;
+    }
+
+    public ArrayList<Media> getWatchedMovie() {
+        return watchedMovies;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Username: " + userName +
+                "\nSaved movies: " + savedMedia.size() +
+                "\nWatched movies: " + watchedMovies.size();
+    }
+
 }
