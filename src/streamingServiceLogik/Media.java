4c0ead72fe496d3080a6e042727fc8075ce17dac
eminkilic <kilicemin404@gmail.com>
2026-04-22 10:53:18 +0200

    dasda

--- src/streamingServiceLogik/Media.java
+++ src/streamingServiceLogik/Media.java
@@ -14,56 +14,57 @@ public class Media {
     private String onAir;
     private String seasons;
 
-    //Constructerene for movie&series (lavede det for at prøve fileReaderen)
-    //String type skal skiftes ud med Enum på begge
-    //Film csv eksempel: The Godfather; 1972; Crime, Drama; 9,2;
-    //Serires csv eksempel: Twin Peaks; 1990-1991; Crime, Drama, Mystery; 8,8; 1-8, 2-22;
 
     public Media(String title, String releaseYear, ArrayList<Category> categories, double rating) {
-            this.title = title;
-            this.releaseYear = releaseYear;
-            this.categories = categories;
-            this.rating = rating;
-        }
+        this.title = title;
+        this.releaseYear = releaseYear;
+        this.categories = categories;
+        this.rating = rating;
+    }
 
-    public Media(String title, String onAir, ArrayList < Category > categories, double rating, String seasons){
-            this.title = title;
-            this.onAir = onAir;
-            this.categories = categories;
-            this.rating = rating;
-            this.seasons = seasons;
-        }
-
-
-        public String getTitle () {
-            return title;
-        }
-
-        public double getRating () {
-            return rating;
-        }
+    public Media(String title, String onAir, ArrayList<Category> categories, double rating, String seasons) {
+        this.title = title;
+        this.onAir = onAir;
+        this.categories = categories;
+        this.rating = rating;
+        this.seasons = seasons;
+    }
 
 
-        public String getReleaseYear () {
-            return releaseYear;
-        }
+    public String getTitle() {
+        return title;
+    }
 
-        public String getOnAir () {
-            return onAir;
+    public double getRating() {
+        return rating;
+    }
 
-        }
-        public String getSeasons () {
-            return seasons;
-        }
+
+    public String getReleaseYear() {
+        return releaseYear;
+    }
+
+    public String getOnAir() {
+        return onAir;
+
+    }
+
+    public String getSeasons() {
+        return seasons;
+    }
 
     public ArrayList<Category> getCategories() {
         return categories;
     }
 
-    public String toString () {
-            return title;
-            // ikke færdig
+
+    @Override
+    public String toString() {
+        if (seasons != null) {
+            return title + " " + onAir + " Rating: " + rating + " Kategorier: " + categories + " Sæsoner: " + seasons;
         }
+        return title + " " + releaseYear + " Rating: " + rating + " Kategorier: " + categories;
+    }
 
         public void play (User user) {
             System.out.println(title + "afspilles nu ");
